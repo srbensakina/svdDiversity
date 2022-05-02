223a9418ebd5981d905b1a81d83a12cfc5cc4399
srbensakina <rbensakina16@gmail.com>
2022-05-02 19:51:03 +0100

    changed diversity approach

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ userId= 21
 top_20 = predict_movie_and_rating(userId)
 kDiversifiedItems = 4
 diversifyCandidates(top_20,4,mf.Q)
-#print("M main",mf.Q[92])
-#print(mf.Q)
-# from DiversityUtils import cosdis,word2vec
-#
-# restaurant = "Restaurant"
-# musee = "Musee"
-# hotel = "Hotel"
-#
-# print("res muse",cosdis(word2vec(restaurant),word2vec(musee)))
-# print("res hot",cosdis(word2vec(restaurant),word2vec(hotel)))
+
 
 
